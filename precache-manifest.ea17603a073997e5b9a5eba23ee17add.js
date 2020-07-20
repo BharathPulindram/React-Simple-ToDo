fd0199be26808b956c44a915ddedb910f4e7dc2f
Bharath Kumar <pbkbharath1707@gmail.com>
2020-07-20 17:10:28 +0530

    Updates

---
+++ precache-manifest.ea17603a073997e5b9a5eba23ee17add.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8386e3a1923556049ebe9e4b6906e1e",
+    "url": "/React-Simple-ToDo/index.html"
+  },
+  {
+    "revision": "4c481e45a540b8753948",
+    "url": "/React-Simple-ToDo/static/css/main.cadeb0d5.chunk.css"
+  },
+  {
+    "revision": "5ff32cbd7950fc098552",
+    "url": "/React-Simple-ToDo/static/js/2.0538fec5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Simple-ToDo/static/js/2.0538fec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c481e45a540b8753948",
+    "url": "/React-Simple-ToDo/static/js/main.6efb8d48.chunk.js"
+  },
+  {
+    "revision": "e3ea7608ed19e3f55e76",
+    "url": "/React-Simple-ToDo/static/js/runtime-main.e26f0d49.js"
+  }
+]);
